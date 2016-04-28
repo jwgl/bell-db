27fc2f52ade1de28b89292c4f5958497a3fbd381
Yang Lin <todoubaba@gmail.com>
2016-04-28 22:52:34 +0800

    修正同步脚本 (#5)
    * 修正同步脚本
    
    * 在program_course上创建唯一索引

--- ea/ea-modify.sql
+++ ea/ea-modify.sql
@@ -1,5 +1,1 @@
-create unique index uk_program_course_direction_not_null on ea.program_course(program_id, direction_id, course_id)
-where direction_id is not null;
-
-create unique index uk_program_course_direction_is_null on ea.program_course(program_id, course_id)
-where direction_id is null;
+create unique index uk_program_course_direction on ea.program_course(program_id, coalesce(direction_id, 0), course_id);
