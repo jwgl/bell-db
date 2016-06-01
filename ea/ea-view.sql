e94d947adabb1348adf66a2b5873cdd577f79346
Yang Lin <todoubaba@gmail.com>
2016-06-02 04:48:05 +0800

    修复Meennuu架构错误

--- ea/ea-view.sql
+++ ea/ea-view.sql
@@ -5,14 +5,14 @@ with recursive r as (
         m.id as root,
         1 as path_level,
         to_char(m.display_order, '09') as display_order
-    from tm.menu m
+    from ea.menu m
     where array_length(regexp_split_to_array(m.id, E'\\.'), 1) = 1
     union all
     select m.id, m.name, m.label_cn, m.label_en,
         r.root as root,
         array_length(regexp_split_to_array(m.id, E'\\.'), 1),
         r.display_order || to_char(m.display_order, '09')
-    from tm.menu m
+    from ea.menu m
     join r on strpos(m.id, r.id) = 1 and array_length(regexp_split_to_array(m.id, E'\\.'), 1) = r.path_level + 1
 )
 select id, name, label_cn, label_en, path_level -1 as menu_level, root from r
