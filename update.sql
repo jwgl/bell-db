17fb10e5f7c0cd3f057ad7e5c10ef24236c641d6
Yang Lin <todoubaba@gmail.com>
2016-11-03 20:07:13 +0800

    删除已更新数据

--- update.sql
+++
@@ -1,5 +0,0 @@
-Hibernate: create table ea.term_swap_date (id int8 not null, from_date date not null, term_id int4 not null, to_date date not null, primary key (id))
-Hibernate: comment on table ea.term_swap_date is '校历调换日期'
-Hibernate: comment on column ea.term_swap_date.from_date is '源日期'
-Hibernate: comment on column ea.term_swap_date.to_date is '目标日期'
-Hibernate: alter table ea.term_swap_date add constraint FKiiylyhqj638sl3b4bthdly2dk foreign key (term_id) references ea.term
