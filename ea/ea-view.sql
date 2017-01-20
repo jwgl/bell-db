cdc6b5cd359dcec85d7495788360f7954debbf71
Yang Lin <todoubaba@gmail.com>
2017-01-21 01:55:02 +0800

    Task schedule view include course item

--- ea/ea-view.sql
+++ ea/ea-view.sql
@@ -38,11 +38,12 @@ group by term_id, task.id, c.id, c.name, ci.name, task.code;
 
 -- 教学安排
 create or replace view ea.av_task_schedule as
-select a.id, cc.term_id, c.id as course_id, c.name as course_name,
+select a.id, cc.term_id, c.id as course_id, c.name as course_name, ci.name as course_item,
     te.id as teacher_id, te.name as teacher_name, a.start_week, a.end_week,
     day_of_week, start_section, total_section, odd_even, place_id, task_id, task.code as task_code, course_class_id
 from task_schedule a
 join task on a.task_id = task.id
 join course_class cc on cc.id = task.course_class_id
 join course c on c.id = cc.course_id
-join teacher te on te.id = a.teacher_id;
+join teacher te on te.id = a.teacher_id
+left join course_item ci on ci.id = task.course_item_id;
