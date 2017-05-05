84837829a0f66dd52254eabd06fbe82be1bfd478
Yang Lin <todoubaba@gmail.com>
2017-05-06 02:57:39 +0800

    Remove tm reference when sync (#62)

---
+++ tm/tm-grant.sql
@@ -0,0 +1,8 @@
+/**
+ * database bell/tm
+ */
+grant usage on schema tm to ea;
+
+grant select, delete on student_leave_item to ea;
+grant select, delete on free_listen_item   to ea;
+grant select, delete on rollcall           to ea;
