d04714f5b9a569f96e608fc62e45de2e46eb9209
jerry <17481452@qq.com>
2017-06-08 16:33:10 +0800

    debug tm-view.sql

--- tm/tm-view.sql
+++ tm/tm-view.sql
@@ -62,14 +62,11 @@ union all
 select t.id as user_id, 'ROLE_STUDENT_COUNSELLOR' as role_id
 from ea.teacher t
 where exists(select * from admin_class_at_school where counsellor_id = t.id)
-UNION ALL
-SELECT DISTINCT s.teacher_id AS user_id,
-'ROLE_OBSERVER' AS role_id
-FROM tm.observer s
-JOIN tm.observer_type r ON s.observer_type_id = r.id
-JOIN ea.term t ON s.term_id = t.id
-WHERE (r.name = '校督导' OR r.name = '院督导') AND t.active IS TRUE;
-;
+union all
+select distinct s.teacher_id as user_id, 'ROLE_OBSERVER' as role_id
+from tm.observer s
+join ea.term t on s.term_id = t.id
+where t.active is true;
 
 -- 学生角色
 create or replace view tm.dv_student_role as
@@ -321,158 +318,114 @@ join ea.course_class on task.course_class_id = course_class.id
 where rollcall.type <> 6;
 
 --引用时段表;
-CREATE OR REPLACE VIEW tm.dv_course_section AS
-SELECT *
-FROM tm.booking_section
-WHERE booking_section.id <> 0 AND booking_section.id <> '-5'::integer;
+create or replace view tm.dv_course_section as
+select *
+from tm.booking_section
+where booking_section.id <> 0 and booking_section.id <> '-5'::integer;
 
 -- 统计老师被校督导听课次数（与课程无关）;
-CREATE OR REPLACE VIEW tm.dv_observation_count AS
-SELECT teacher.id AS teacher_id,
-       teacher.name AS teacher_name,
-       department.name AS department_name,
-       count(*) AS supervise_count
-FROM tm.observation_form form
-JOIN ea.teacher ON form.teacher_id = teacher.id
-JOIN ea.term ON form.term_id = term.id
-JOIN ea.department ON teacher.department_id = department.id
-WHERE term.active IS TRUE AND form.observer_type = 1
-GROUP BY teacher.id, teacher.name, department.name
-HAVING count(*) > 1;
+create or replace view tm.dv_observation_count as
+select teacher.id as teacher_id,
+       teacher.name as teacher_name,
+       department.name as department_name,
+       count(*) as supervise_count
+from tm.observation_form form
+join ea.teacher on form.teacher_id = teacher.id
+join ea.term on form.term_id = term.id
+join ea.department on teacher.department_id = department.id
+where term.active is true and form.observer_type = 1
+group by teacher.id, teacher.name, department.name
+having count(*) > 1;
 
 -- 历史遗留数据视图，避免修改;
-CREATE OR REPLACE VIEW tm.dv_observation_legacy_form AS
-SELECT *
-FROM tm.observation_legacy_form form;
+create or replace view tm.dv_observation_legacy_form as
+select *
+from tm.observation_legacy_form form;
 
 -- 优先听课名单视图:本学期有课，且查询的当周还有课，近4个学期未被听课，是否新老师;
-CREATE OR REPLACE VIEW tm.dv_observation_priority AS
-WITH active_term AS (
-    SELECT term.id
-    FROM ea.term
-    WHERE term.active IS TRUE
-), course_teacher AS (
-    SELECT DISTINCT courseclass.teacher_id, courseclass.term_id AS termid
-    FROM ea.task_schedule schedule
-    JOIN ea.task task ON schedule.task_id = task.id
-    JOIN ea.course_class courseclass ON task.course_class_id = courseclass.id
-    JOIN ea.course course_1 ON courseclass.course_id = course_1.id
-), active_teacher AS (
-    SELECT DISTINCT courseteacher.id AS teacher_id,
-          courseteacher.name AS teacher_name,
+create or replace view tm.dv_observation_priority as
+with active_term as (
+    select term.id
+    from ea.term
+    where term.active is true
+), course_teacher as (
+    select distinct courseclass.teacher_id, courseclass.term_id as termid
+    from ea.task_schedule schedule
+    join ea.task task on schedule.task_id = task.id
+    join ea.course_class courseclass on task.course_class_id = courseclass.id
+    join ea.course course_1 on courseclass.course_id = course_1.id
+), active_teacher as (
+    select distinct courseteacher.id as teacher_id,
+          courseteacher.name as teacher_name,
           courseteacher.academic_title,
-          department.name AS department_name,
-          courseclass.term_id AS termid
-    FROM ea.task_schedule schedule
-    JOIN ea.task task ON schedule.task_id = task.id
-    JOIN ea.course_class courseclass ON task.course_class_id = courseclass.id
-    JOIN ea.course course_1 ON courseclass.course_id = course_1.id
-    JOIN ea.teacher courseteacher ON courseclass.teacher_id = courseteacher.id
-    JOIN ea.department department ON courseteacher.department_id = department.id
-    WHERE courseclass.term_id = ((SELECT active_term.id FROM active_term))
-    AND schedule.end_week > (( SELECT date_part('week', now()) - date_part('week', term.start_date) + 1 FROM ea.term WHERE term.active IS TRUE))
-), new_teacher AS (
-    SELECT course_teacher.teacher_id
-    FROM course_teacher
-    GROUP BY course_teacher.teacher_id
-    HAVING min(course_teacher.termid) = (( SELECT active_term.id FROM active_term))
-), inspect4 AS (
-    SELECT DISTINCT inspector.teachercode AS teacher_id
-    FROM tm.observation_legacy_form inspector
-    WHERE inspector.teachercode IS NOT NULL AND inspector.type = '督导' AND (inspector.term_id + 20) > (( SELECT active_term.id FROM active_term))
-    UNION
-    SELECT DISTINCT form.teacher_id
-    FROM tm.observation_form form
-    JOIN ea.task_schedule schedule ON form.task_schedule_id = schedule.id
-    JOIN ea.task ON schedule.task_id = task.id
-    JOIN ea.course_class courseclass ON task.course_class_id = courseclass.id
-    WHERE form.observer_type =1 AND (courseclass.term_id + 20) > (( SELECT active_term.id FROM active_term))
+          department.name as department_name,
+          courseclass.term_id as termid
+    from ea.task_schedule schedule
+    join ea.task task on schedule.task_id = task.id
+    join ea.course_class courseclass on task.course_class_id = courseclass.id
+    join ea.course course_1 on courseclass.course_id = course_1.id
+    join ea.teacher courseteacher on courseclass.teacher_id = courseteacher.id
+    join ea.department department on courseteacher.department_id = department.id
+    where courseclass.term_id = ((select active_term.id from active_term))
+    and schedule.end_week > (( select date_part('week', now()) - date_part('week', term.start_date) + 1 from ea.term where term.active is true))
+), new_teacher as (
+    select course_teacher.teacher_id
+    from course_teacher
+    group by course_teacher.teacher_id
+    having min(course_teacher.termid) = (( select active_term.id from active_term))
+), inspect4 as (
+    select distinct inspector.teachercode as teacher_id
+    from tm.observation_legacy_form inspector
+    where inspector.teachercode is not null and inspector.observer_type = 1 and (inspector.term_id + 20) > (( select active_term.id from active_term))
+    union
+    select distinct form.teacher_id
+    from tm.observation_form form    
+    where form.observer_type =1 and (form.term_id + 20) > (( select active_term.id from active_term))
 )
-SELECT DISTINCT active.teacher_id,
+select distinct active.teacher_id,
     active.teacher_name,
     active.department_name,
     active.academic_title,
-    a.teacher_id AS isnew,
-    inspect4.teacher_id AS has_supervisor
-FROM active_teacher active
-LEFT JOIN new_teacher a ON active.teacher_id = a.teacher_id
-LEFT JOIN inspect4 ON active.teacher_id = inspect4.teacher_id;
-
- -- JOIN课表，抽取最全常用字段;
-CREATE OR REPLACE VIEW tm.dv_observation_view AS
-SELECT form.id,
-    form.attendant_stds,
-    form.due_stds,
-    form.earlier,
-    form.evaluate_level,
-    form.evaluation_text,
-    form.late,
-    form.late_stds,
-    form.leave,
-    form.leave_stds,
-    form.lecture_week,
-    form.status,
-    form.suggest,
-    supervisor.id AS supervisor_id,
-    form.supervisor_date,
-    form.teaching_methods,
-    form.total_section AS form_total_section,
-    form.record_date,
-    form.reward_date,
-    supervisor.name AS supervisor_name,
-    form.observer_type,
-    courseteacher.id AS teacher_id,
-    courseteacher.academic_title,
-    courseclass.name AS course_class_name,
-    schedule.start_week,
-    schedule.end_week,
-    schedule.odd_even,
-    schedule.day_of_week,
-    schedule.start_section,
-    schedule.total_section,
-    course_1.name AS course_name,
-    place.name AS place_name,
-    courseteacher.name AS teacher_name,
-    department.name AS department_name,
-    courseclass.term_id AS termid
-FROM tm.observation_form form
-JOIN ea.teacher supervisor ON form.observer_id = supervisor.id
-JOIN ea.task_schedule schedule ON form.task_schedule_id = schedule.id
-JOIN ea.task task ON schedule.task_id = task.id
-JOIN ea.course_class courseclass ON task.course_class_id = courseclass.id
-JOIN ea.department department ON courseclass.department_id = department.id
-JOIN ea.course course_1 ON courseclass.course_id = course_1.id
-JOIN ea.teacher courseteacher ON courseclass.teacher_id = courseteacher.id
-LEFT JOIN ea.place ON schedule.place_id = place.id
-ORDER BY id;
+    a.teacher_id as isnew,
+    inspect4.teacher_id as has_supervisor
+from active_teacher active
+left join new_teacher a on active.teacher_id = a.teacher_id
+left join inspect4 on active.teacher_id = inspect4.teacher_id; 
 
 -- 督导听课视图，合并了新旧数据，只抽取重要的字段信息;
-CREATE OR REPLACE VIEW tm.dv_observation_public AS
-SELECT view.id,
-    false AS is_legacy,
+create or replace view tm.dv_observation_public as
+select view.id,
+    false as is_legacy,
     view.supervisor_date,
     view.evaluate_level,
-    concat(substring('校院',view.observer_type,1),'督导'),
-    view.termid AS term_id,
+    view.observer_type,
+    view.termid as term_id,
     view.department_name,
     view.teacher_id,
     view.teacher_name,
     view.course_name,
-    concat('星期', substring('一二三四五六日', view.day_of_week, 1), ' ', view.start_section, '-', (view.start_section + view.total_section - 1), '节 ', view.place_name) AS course_other_info
-FROM tm.dv_observation_view view
-WHERE view.status = 2
-UNION ALL
-SELECT legacy_form.id,
-    true AS is_legacy,
-    legacy_form.listentime AS supervisor_date,
-    legacy_form.evaluategrade AS evaluate_level,
-    legacy_form.type AS type_name,
+    view.place_name,
+    view.day_of_week,
+    view.start_section,
+    view.start_section,
+    view.total_section
+from tm.dv_observation_view view
+where view.status = 2
+union all
+select legacy_form.id,
+    true as is_legacy,
+    legacy_form.listentime as supervisor_date,
+    legacy_form.evaluategrade as evaluate_level,
+    legacy_form.observer_type,
     legacy_form.term_id,
-    legacy_form.collegename AS department_name,
-    legacy_form.teachercode AS teacher_id,
-    legacy_form.teachername AS teacher_name,
-    legacy_form.coursename AS course_name,
-    legacy_form.classpostion AS course_other_info
-FROM tm.dv_observation_legacy_form legacy_form
-WHERE legacy_form.state = 'yes';
-
+    legacy_form.collegename as department_name,
+    legacy_form.teachercode as teacher_id,
+    legacy_form.teachername as teacher_name,
+    legacy_form.coursename as course_name,
+    legacy_form.classpostion as place,
+    null as day_of_week,
+    null as start_section,
+    null as start_section,
+    null as total_section
+from tm.dv_observation_legacy_form legacy_form
+where legacy_form.state = 'yes';
