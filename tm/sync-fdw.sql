d6a415d8824e87d81195154ace2ef3cbb5e5bac4
Zhong Qin <17481452@qq.com>
2017-12-21 15:07:43 +0800

    Dual degree (#99)

--- tm/sync-fdw.sql
+++ tm/sync-fdw.sql
@@ -110,3 +110,24 @@ create foreign table tm.et_task_student (
     exam_flag varchar(10),
     operator varchar(5)
 ) server zf options (schema 'TM', table 'IV_TASK_STUDENT');
+
+-- 自助打印系统学生名单，用于学生打印四分制成绩单
+drop foreign table if exists tm.et_dualdegree_student;
+create foreign table tm.et_dualdegree_student(
+    id integer, 
+    student_id varchar(20), 
+    student_name varchar(50), 
+    date_created timestamp, 
+    date_deleted timestamp, 
+    creator varchar(20), 
+    deleter varchar(20), 
+    is_dualdegree integer, 
+    region varchar(20)
+) server zf_print options (schema 'BNUEP_PRINT', table 'T_ZZ_XSMD');
+
+-- 自助打印系统教学计划号与项目名称对应表，用于学生打印四分制成绩单
+drop foreign table if exists tm.et_dualdegree_major_region;
+create foreign table tm.et_dualdegree_major_region(
+    major_id varchar(10), 
+    region varchar(50)
+) server zf_print options (schema 'BNUEP_PRINT', table 'T_TMS_PROJECT');
--- tm/tm-view.sql
+++ tm/tm-view.sql
@@ -73,7 +73,10 @@ union all
 select distinct s.teacher_id as user_id, 'ROLE_OBSERVER' as role_id
 from tm.observer s
 join ea.term t on s.term_id = t.id
-where t.active is true;
+where t.active is true
+union all
+select distinct s.teacher_id as user_id, 'ROLE_DUALDEGREE_ADMIN_DEPT' as role_id
+from tm.dual_degree_dept_admin s;
 
 -- 学生角色
 create or replace view tm.dv_student_role as
@@ -434,6 +437,7 @@ select distinct form.id,
     place.name as place_name,
     courseteacher.name as teacher_name,
     department.name as department_name,
+    cp.property_name as property,
     courseclass.term_id as term_id
    from tm.observation_form form
      join ea.teacher supervisor on form.observer_id = supervisor.id
@@ -450,6 +454,7 @@ select distinct form.id,
      join ea.course course_1 on courseclass.course_id = course_1.id
      join ea.teacher courseteacher on courseclass.teacher_id = courseteacher.id
      left join ea.place on schedule.place_id = place.id
+     join tm.dv_observation_course_property cp on courseclass.id = cp.id
   where form.term_id=courseclass.term_id
   order by form.id;
 
@@ -470,7 +475,7 @@ select view.id,
     view.start_section,
     view.total_section
 from tm.dv_observation_view view
-where view.status = 2
+where view.status = 2;
 
 -- 课程性质视图
 create or replace view tm.dv_observation_course_property as
