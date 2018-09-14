7e42f91fa7f0434ebd4a16bcb7afd8bde1056d19
todoubaba <todoubaba@gmail.com>
2018-09-14 16:06:34 +0800

    Add place to task schedule view

--- ea/ea-view.sql
+++ ea/ea-view.sql
@@ -76,14 +76,16 @@ group by term_id, task.id, c.id, c.name, ci.name, task.code;
 create or replace view ea.av_task_schedule as
 select a.id, cc.term_id, c.id as course_id, c.name as course_name, ci.name as course_item,
     te.id as teacher_id, te.name as teacher_name, a.start_week, a.end_week,
-    day_of_week, start_section, total_section, odd_even, place_id, task_id, task.code as task_code, course_class_id, d.name as department
+    day_of_week, start_section, total_section, odd_even, place_id, place.name as place
+    task_id, task.code as task_code, course_class_id, d.name as department
 from task_schedule a
 join task on a.task_id = task.id
 join course_class cc on cc.id = task.course_class_id
 join department d on d.id = cc.department_id
 join course c on c.id = cc.course_id
 join teacher te on te.id = a.teacher_id
-left join course_item ci on ci.id = task.course_item_id;
+left join course_item ci on ci.id = task.course_item_id
+left join place on place.id = a.place_id;
 
 -- 学生课表
 create or replace view ea.av_student_schedule as
