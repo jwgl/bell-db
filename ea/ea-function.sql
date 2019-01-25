b8f96e8e1dc232ed6c81e1f2acf79ff60d46208a
Yang Lin <todoubaba@gmail.com>
2019-01-25 12:50:17 +0800

    Add task schedule member functions (#137)

--- ea/ea-function.sql
+++ ea/ea-function.sql
@@ -51,9 +51,55 @@ begin
   elseif odd_even = 2 then
     return result & 715827882;  -- x'2AAAAAAA'
   else
-    raise 'Error odd even value';
+    raise 'Error odd_even value';
   end if;
 end;
+$$ language plpgsql;
+
+/**
+ * 周次转整数(成员函数)
+ */
+create or replace function ea.mf_get_weeks(
+  schedule ea.task_schedule
+) returns integer immutable
+returns null on null input as $$
+begin
+  return ea.fn_weeks_to_integer(schedule.start_week, schedule.end_week, schedule.odd_even);
+end
+$$ language plpgsql;
+
+/**
+ * 计算周数
+ */
+create function ea.fn_weeks_count(
+  start_week integer,
+  end_week integer,
+  odd_even integer
+) returns integer immutable
+returns null on null input as $$
+begin
+  if odd_even = 0 then
+    return end_week - start_week + 1;
+  elseif odd_even = 1 then
+    return (end_week - start_week + 1) / 2 + (end_week & 1) * (start_week & 1);
+  elseif odd_even = 2 then
+    return (end_week - start_week + 1) / 2 + ((end_week + 1) & 1) * ((start_week + 1) & 1);
+  else
+    raise 'Error odd_even value';
+  end if;
+end
+$$ language plpgsql;
+
+/**
+ * 计算周数(成员函数)
+ */
+create function ea.mf_get_week_count(
+  schedule ea.task_schedule
+) returns integer immutable
+returns null on null input as $$
+begin
+  return ea.fn_weeks_count(schedule.start_week, schedule.end_week, schedule.odd_even);
+end
 $$ language plpgsql;
 
 /*
@@ -67,6 +113,18 @@ returns null on null input as $$
 begin
   return 2147483647 >> (31 - total_section) << (start_section - 1);
 end;
+$$ language plpgsql;
+
+/*
+ * 节次转整数(成员函数)
+ */
+create function ea.mf_get_sections(
+  schedule ea.task_schedule
+) returns integer immutable
+returns null on null input as $$
+begin
+  return ea.fn_sections_to_integer(schedule.start_section, schedule.total_section);
+end
 $$ language plpgsql;
 
 /*
@@ -85,6 +143,38 @@ begin
   return start_week || '-' || end_week || '周'
     || case odd_even when 1 then '(单)' when 2 then '(双)' else '' end
     || '星期' || substring('一二三四五六日', day_of_week, 1)
-    || start_section || '-' || (start_section + total_section - 1) || '节';
+    || start_section || '-' || start_section + total_section - 1 || '节';
 end;
 $$ language plpgsql;
+
+/*
+ * 上课时间(成员函数)
+ */
+create function ea.mf_get_timetable_string(
+  schedule ea.task_schedule
+) returns text immutable
+returns null on null input as $$
+begin
+  return ea.fn_timetable_to_string(
+    schedule.start_week,
+    schedule.end_week,
+    schedule.odd_even,
+    schedule.day_of_week,
+    schedule.start_section,
+    schedule.total_section
+  );
+end
+$$ language plpgsql;
+
+create or replace function ea.mf_get_timetable_ordinal(
+  schedule ea.task_schedule
+) returns integer immutable
+returns null on null input as $$
+begin
+  return schedule.day_of_week * 100000 +
+         schedule.start_section * 1000 +
+         schedule.start_week * 10 +
+         schedule.odd_even;
+end;
+$$ language plpgsql;
+
