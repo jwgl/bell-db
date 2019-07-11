e35ef349eaa8f8dffff5caee088ac1ba22f1cc6e
jerry <17481452@qq.com>
2019-07-11 17:02:14 +0800

    更改角色名，更改同步视图部门有效性逻辑

--- ea/sync-view.sql
+++ ea/sync-view.sql
@@ -84,12 +84,12 @@ select xydm as id,
     end as has_students, -- 是否有学生
     case
         when xylx is null
-            or not exists (select 1 from zfxfzb.xsjbxxb xs where xs.xy = xymc)
+            or not exists (select 1 from zfxfzb.xsjbxxb xs where xs.xy = xymc and xjzt = '有')
             or xylx = '教学单位'
             and exists (select 1 from zfxfzb.xsjbxxb xs where xs.dqszj >= 2010 and xs.xy = xymc)
             then 1
         else 0
-    end as enabled -- 近四年没有学生则禁用
+    end as enabled 
 from zfxfzb.xydmb
 order by id;
 
--- tm/tm-steer-init.sql
+++ tm/tm-steer-init.sql
@@ -5,7 +5,7 @@ INSERT INTO tm.role (id,name) VALUES ('ROLE_OBSERVATION_ADMIN',          '督导
 --创建权限
 INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVATION_WRITE',              '督导听课-编辑');
 INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVATION_READ',               '督导听课-查看');
-INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVATION_DEPT_APPROVE',       '督导听课-院督导听课发布');
+INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVATION_APPROVE',       	   '督导听课-院督导听课发布');
 INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVER_ADMIN',                 '督导管理');
 INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVER_DEPT_ADMIN',            '督导管理-院督导员管理');
 
@@ -13,8 +13,8 @@ INSERT INTO tm.permission (id,name) VALUES ('PERM_OBSERVER_DEPT_ADMIN',         
 INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_OBSERVER',                   'PERM_OBSERVATION_WRITE');
 INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_OBSERVATION_ADMIN',          'PERM_OBSERVATION_WRITE');
 INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_OBSERVATION_ADMIN',          'PERM_OBSERVER_ADMIN');
-INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_OBSERVATION_ADMIN',          'PERM_OBSERVATION_DEPT_APPROVE');
-INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_DEAN_OF_TEACHING',           'PERM_OBSERVATION_DEPT_APPROVE');
+INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_OBSERVATION_ADMIN',          'PERM_OBSERVATION_APPROVE');
+INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_DEAN_OF_TEACHING',           'PERM_OBSERVATION_APPROVE');
 INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_ACADEMIC_SECRETARY',         'PERM_OBSERVER_DEPT_ADMIN');
 INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_COURSE_CLASS_TEACHER',       'PERM_OBSERVATION_READ');
 INSERT INTO tm.role_permission (role_id,permission_id) VALUES ('ROLE_ONCE_COURSE_CLASS_TEACHER',  'PERM_OBSERVATION_READ');
@@ -35,4 +35,4 @@ insert into tm.menu_item(id, menu_id, label, url, enabled, depends_on, display_o
 insert into tm.menu_item(id, menu_id, label, url, enabled, depends_on, display_order, permission_id) values
 ('main.settings.deptObserver', 'main.settings', '院级督导', '/steer/departments/${departmentId}/observers', true, array['TM-STEER-API'], 41, 'PERM_OBSERVER_DEPT_ADMIN');
 insert into tm.menu_item(id, menu_id, label, url, enabled, depends_on, display_order, permission_id) values
-('main.steer.dean', 'main.steer', '本学院被听课', '/steer/obervers/${userId}/deans', true, array['TM-STEER-API'], 14, 'PERM_OBSERVATION_DEPT_APPROVE');
+('main.steer.dean', 'main.steer', '本学院被听课', '/steer/obervers/${userId}/deans', true, array['TM-STEER-API'], 14, 'PERM_OBSERVATION_APPROVE');
